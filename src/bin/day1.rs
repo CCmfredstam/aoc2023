37c5b819f89dca2b48bb9f1e623e772717b34738
Marcus Fredstam <marcus.fredstam@crowdcollective.com>
2023-11-30 22:09:51 +0100

    2023 day 1 part 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,13 +1,18 @@
 use std::fs::read_to_string;
 
 fn main() {
-    println!("Hello, day1!");
     let data = read_to_string("input/day1.txt").unwrap();
-    let lines: Vec<String> = data.split("\n").filter(|s| !s.is_empty()).map(|s| s.to_string()).collect();
-    print!("String print: ");
-    println!("{}", data);
-    print!("Lines print: ");
+    let lines: Vec<String> = data.split('\n').filter(|s| !s.is_empty()).map(|s| s.to_string()).collect();
+
+    let mut sum_calibration = 0;
+
     for line in lines {
-        println!("{}", line)
+        let num_line: String = line.chars().filter(|c| c.is_ascii_digit()).collect();
+        let first_digit = num_line.chars().next().unwrap();
+        let last_digit = num_line.chars().last().unwrap();
+        let calibration = format!("{}{}", first_digit, last_digit).parse::<i32>().unwrap();
+        sum_calibration += calibration;
     }
+
+    println!("{}", sum_calibration);
 }
