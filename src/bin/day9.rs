7ce1e61ff87edd497a0ca2c0b63f256384a23dcf
Marcus Fredstam <marcus.fredstam@crowdcollective.com>
2023-12-09 21:46:16 +0100

    2023 day 9 part 1

---
+++ src/bin/day9.rs
@@ -0,0 +1,65 @@
+use std::fs::read_to_string;
+
+
+fn extrapolate(dataset: String) -> i64 {
+    let datapoints: Vec<i64> = dataset.split_whitespace().map(|s|s.parse().unwrap()).collect();
+
+    let mut extrapolation = *datapoints.last().unwrap();
+
+    let mut diffs: Vec<i64> = datapoints.windows(2).map(|x| x[1] - x[0]).collect();
+
+    extrapolation += diffs.last().unwrap();
+
+    loop {
+        diffs = diffs.windows(2).map(|x| x[1] - x[0]).collect();
+        if diffs.iter().all(|x| *x == 0) {
+            return extrapolation;
+        }
+        extrapolation += diffs.last().unwrap();
+    }
+
+}
+
+fn main_part1() {
+    // Read todays input
+    let data = read_to_string("input/day9.txt").unwrap();
+    let lines: Vec<String> = data.split('\n').filter(|s| !s.is_empty()).map(|s| s.to_string()).collect();
+    let _lines: Vec<String> = vec!["0 3 6 9 12 15".to_string(),
+                                  "1 3 6 10 15 21".to_string(),
+                                  "10 13 16 21 30 45".to_string(),
+                                ];
+
+    let mut total_extrapolation: i64 = 0;
+    for line in lines {
+        let next_point = extrapolate(line);
+        total_extrapolation += next_point;
+    }
+
+    println!("Part1: {}", total_extrapolation);
+
+}
+
+fn main_part2() {
+    // Read todays input
+    let data = read_to_string("input/day9.txt").unwrap();
+    let _lines: Vec<String> = data.split('\n').filter(|s| !s.is_empty()).map(|s| s.to_string()).collect();
+
+    println!("Part2: {}", 0);
+
+}
+
+fn main() {
+    main_part1();
+    main_part2();
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_placeholder() {
+    }
+}
