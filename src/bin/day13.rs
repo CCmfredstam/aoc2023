0f1be4560383633bd7f29be40f65f1d6277b020f
Marcus Fredstam <marcus.fredstam@crowdcollective.com>
2023-12-13 13:56:24 +0100

    day13 part 1, rows seem to sum correct

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -14,12 +14,38 @@ impl Group {
         0
     }
 
+    fn row_pattern_correct(&self, idx: usize) -> bool {
+        for row_idx in idx..self.ground.len() {
+            let rev_idx = (idx as i64 - 1 - (row_idx as i64 - idx as i64)) as i64;
+            if rev_idx < 0 {
+                return true;
+            }
+            if self.ground[row_idx] != self.ground[rev_idx as usize] {
+                return false;
+            }
+        }
+        true
+    }
+
     fn summarize_rows(&self) -> i64 {
-        0 * 100
+        let mut row_value: i64 = 0;
+        for row_idx in 1..self.ground.len() {
+            if self.ground[row_idx] == self.ground[row_idx-1] {
+                if self.row_pattern_correct(row_idx) {
+                    row_value = row_idx as i64;
+                    break;
+                }
+            }
+        }
+        row_value * 100
     }
 
     fn summarize_pattern(&self) -> i64 {
-        self.summarize_rows() + self.summarize_columns()
+        let x = self.summarize_rows();
+        let y = self.summarize_columns();
+        dbg!("ROW:", x, "COL:", y);
+        x + y
+        //self.summarize_rows() + self.summarize_columns()
     }
 }
 
@@ -48,17 +74,17 @@ fn parse_puzzle_input(lines: &Vec<String>) -> Vec<Group> {
 
 fn main_part1() {
     // Read todays input
-    let _data = read_to_string("input/day13.txt").unwrap();
-    let _lines: Vec<String> = _data.split('\n').filter(|s| !s.is_empty()).map(|s| s.to_string()).collect();
-    let data = read_to_string("input/test_input.txt").unwrap();
+    let data = read_to_string("input/day13.txt").unwrap();
     let lines: Vec<String> = data.split('\n').map(|s| s.to_string()).collect();
+    let _data = read_to_string("input/test_input.txt").unwrap();
+    let _lines: Vec<String> = data.split('\n').map(|s| s.to_string()).collect();
 
     let puzzle_groups: Vec<Group> = parse_puzzle_input(&lines);
 
     let mut total_sum = 0;
     for group in puzzle_groups {
         println!("{group:#?}");
-        total_sum = group.summarize_pattern();
+        total_sum += group.summarize_pattern();
     }
 
     println!("Part1: {}", total_sum);
